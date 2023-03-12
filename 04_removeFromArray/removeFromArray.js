b1800c4f05cce8cd3c6b906702bda2981c5bcde0
Amrit57 <fenpadorje7@gmail.com>
2023-03-12 18:49:32 +0545

    all tests from this exercise is complete.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+   let newArray = [];
 
-};
+   array.forEach((item)=> {
 
+      if(!args.includes(item))
+
+      newArray.push(item);
+   })
+return newArray;
+
+}
+console.log(removeFromArray(["hey",2,3,"ho"], 2, "hey"));
 // Do not edit below this line
 module.exports = removeFromArray;
