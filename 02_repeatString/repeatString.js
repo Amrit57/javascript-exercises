5b098ba8cd72c145d4d13d8b75da611b72da4b71
Amrit57 <fenpadorje7@gmail.com>
2023-03-14 11:49:08 +0545

    Fundamentals all js exercises done.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(text,num) {
+let string = ''
+if (num<0){
+    return string = 'ERROR';
+} else{
+    for(i=0;i<num;i++){
+        string += text;
+   }
+}
 
+return string;
 };
+console.log(repeatString("",10));
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    if(word =="") {
+        return word;
+    }
+    else{
+
+        let splitWord = word.split('');
+        let reverseWord = splitWord.reverse();
+        let joinWord = reverseWord.join("");
+        console.log(joinWord)
+        return joinWord;
+    }
 
 };
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
