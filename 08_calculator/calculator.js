3823513d9187f6944621522c676a3f1b99c0633b
Amrit57 <fenpadorje7@gmail.com>
2023-03-22 17:00:56 +0545

    calculator/sum all test complete.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,9 +7,15 @@ const subtract = function(num1, num2) {
 };
 subtract(10,4);
 
-const sum = function() {
-	
+const sum = function([...args]) {
+   let sums=0;
+    for(i = 0; i< args.length; i++){
+      sums += args[i]
+    }
+    console.log(sums);
+ return sums;
 };
+sum([1,3,5,7,9]);
 
 const multiply = function() {
 
---
+++ 08_calculator/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+for(i = 0; i< args.length; i++){
+    //   sums += args[i]
+    // }
