bcf86c10df593a1b3bf4bad72c5dbccce2c2ecd1
Amrit57 <fenpadorje7@gmail.com>
2023-03-23 07:05:43 +0545

    Palindromes all test complete.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toUpperCase()
+    let regex = /[.,\/#!$%\^&\*;:{}=\-_`~()/\s+/g]/g;
+    let clearString = string.replace(regex, "");
+    let newString = clearString.split("").reverse().join("");
+    if (clearString === newString) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+palindromes('ZZZZ car, a man, a maracaz.');
 // Do not edit below this line
 module.exports = palindromes;
