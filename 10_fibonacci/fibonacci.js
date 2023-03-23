d85b63cec3e0e775e1f97b61d86d16aaa093fffc
Amrit57 <fenpadorje7@gmail.com>
2023-03-23 08:03:07 +0545

    Fibonacci a;ll test complete.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
 
+    let n1 = 0; n2 = 1; let nextTerm;
+    if (number < 0) return "OOPS";
+    else if (number === 0) return 0;
+    else {
+        for (i = 1; i <= number; i++) {
+            nextTerm = n1 + n2; //0+1 = 1
+            n1 = n2; // n1 = 1
+            n2 = nextTerm; //n2 = 1
+        }
+        return n1;
+    }
 };
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
